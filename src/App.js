72a60ef64cc5f060f5f3590aea45d9c338003d5d
RAMASUBRAMANIYAN G <gitbash.hub@gmail.com>
2021-01-29 01:39:02 +0530

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './Header'
+import Sidebar from './SideBar'
+import RecommendedVideos from './RecommendedVideos'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Sidebar */}
-      {/* Recommended Video Section */}
+      <div className="app__page">
+      {/* generally uses display: block => stacks everything */}
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 }
---
+++ src/SideBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './SideBar.css'
+import SidebarRow from './SidebarRow'
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+
+function SideBar() {
+    return (
+        <div className="sideBar">
+            <SidebarRow selected Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show More" />
+            <hr />
+        </div>
+    )
+}
+
+export default SideBar
